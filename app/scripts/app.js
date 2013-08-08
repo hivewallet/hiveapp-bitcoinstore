13dea4383254a7a3067cfc51680f451dab7f61cc
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-08-08 10:54:24 +0200

    added underscore

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,6 +7,10 @@ angular.module("hiveBitcoinstoreApp", [])
             templateUrl: "views/categories.html",
             controller: "CategoriesCtrl"
         })
+        .when("/products", {
+            templateUrl: "views/products.html",
+            controller: "ProductsCtrl"
+        })
         .otherwise({
             redirectTo: "/"
         });
