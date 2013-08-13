6cd34e7ea3974bf167faef11577424360e203d38
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-08-13 11:45:05 +0200

    added category list fetched from soap api

--- app/scripts/controllers/category-list.js
+++ app/scripts/controllers/category-list.js
@@ -1,10 +1,17 @@
 "use strict";
 
 angular.module("hiveBitcoinstoreApp")
-    .controller("CategoryListCtrl", function ($scope) {
-        $scope.categories = [
-            "Electronics",
-            "Fashion",
-            "Health"
-        ];
+    .controller("CategoryListCtrl", function ($scope, config) {
+        var client = new MagentoSoapClient(config.storeUrl);
+        client.login(config.storeUsername, config.storePassword).done(function () {
+            client.categoryLevel(config.storeCategoryLevel).done(function (json) {
+                _.each(json.callResponse.callReturn.item, function (item) {
+                    $scope.categories.push({
+                        id:   item.item[0].value.text,
+                        name: item.item[2].value.text
+                    });
+                });
+                $scope.$apply();
+            });
+        });
     });
