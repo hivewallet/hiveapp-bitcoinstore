f0348ae4517f11c1174133dfd4c490cb4be3b524
Szymon Nowak <szimek@gmail.com>
2013-08-13 19:09:30 +0200

    Disable 'Buy' button while processing payment.

--- app/scripts/controllers/product-detail.js
+++ app/scripts/controllers/product-detail.js
@@ -6,9 +6,12 @@ angular.module("hiveBitcoinstoreApp")
 
         $scope.category = $filter('findBy')('category_id', $rootScope.categories, $routeParams.categoryId);
         $scope.product = $filter('findBy')('product_id', $rootScope.products, $routeParams.productId);
+
         if (!$scope.category || !$scope.product) $location.path("/");
 
         $scope.buy = function (productId) {
+            $scope.loading = true;
+
             client.login(config.storeUsername, config.storePassword).done(function () {
                 client.cartCreate().done(function (cartId) {
                     var customer = {
