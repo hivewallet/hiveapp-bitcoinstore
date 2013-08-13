69f87e35e43f5ae883c2011c5192b7408f2333d2
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-08-13 13:44:59 +0200

    added product detail fetched from soap api

--- app/scripts/controllers/product-detail.js
+++ app/scripts/controllers/product-detail.js
@@ -1,16 +1,19 @@
 "use strict";
 
 angular.module("hiveBitcoinstoreApp")
-    .controller("ProductDetailCtrl", function ($scope, $routeParams) {
-        $scope.category = $routeParams.categoryId;
+    .controller("ProductDetailCtrl", function ($scope, $routeParams, config, mapper) {
+        $scope.categoryId = $routeParams.categoryId;
+        $scope.productId = $routeParams.productId;
+
+        var client = new MagentoSoapClient(config.storeUrl);
+        client.login(config.storeUsername, config.storePassword).done(function () {
+            client.productInfo([$scope.productId]).done(function (json) {
+                $scope.product = mapper.build(json.multiCallResponse.multiCallReturn.item);
+                $scope.$apply();
+            });
+        });
 
         $scope.buy = function (productId) {
             alert("You bought " + productId);
         };
-
-        bitcoinstoreApi.getProductDetail($routeParams.productId).done(function (data) {
-            $scope.product = data
-            $scope.$apply(); // TODO: investigate if this is really needed
-        })
-
     });
