edaa0b16dc50c200ff8860676f013d4fa3cf4ef2
Szymon Nowak <szimek@gmail.com>
2013-08-13 17:29:44 +0200

    More cleanup

--- app/scripts/controllers/category-list.js
+++ app/scripts/controllers/category-list.js
@@ -2,8 +2,10 @@
 
 angular.module("hiveBitcoinstoreApp")
     .controller("CategoryListCtrl", function ($scope, $rootScope, config, mapper) {
-        $rootScope.categories = [];
         var client = new MagentoSoapClient(config.storeUrl);
+
+        $rootScope.categories = [];
+
         client.login(config.storeUsername, config.storePassword).done(function () {
             client.categoryLevel(config.storeRootCategoryId).done(function (json) {
                 _.each(json.callResponse.callReturn.item, function (item) {
--- app/scripts/controllers/product-list.js
+++ app/scripts/controllers/product-list.js
@@ -1,54 +1,58 @@
 "use strict";
 
 angular.module("hiveBitcoinstoreApp")
-    .controller("ProductListCtrl", function ($scope, $rootScope, $routeParams, $filter, config, mapper) {
-        var productIds, item, jsonPart;
+    .controller("ProductListCtrl", function ($scope, $rootScope, $routeParams, $location, $filter, config, mapper) {
+        var client = new MagentoSoapClient(config.storeUrl),
+            productIds, item, jsonPart;
 
         $scope.category = $filter('findBy')('category_id', $rootScope.categories, $routeParams.categoryId);
         $rootScope.products = [];
         $scope.currentPage = parseInt($routeParams.page || 1);
 
-        var client = new MagentoSoapClient(config.storeUrl);
-        client.login(config.storeUsername, config.storePassword).done(function () {
-            client.categoryAssignedProducts($scope.category.category_id).done(function (json) {
-                jsonPart = json.callResponse.callReturn.item;
-                jsonPart = (_.isObject(jsonPart) && !_.isArray(jsonPart)) ? [{item: jsonPart.item}] : jsonPart;
-
-                productIds = _.map(jsonPart, function (item) {
-                    return item.item[0].value.text;
-                });
-
-                client.productInfo(productIds).done(function (json) {
-                    jsonPart = json.multiCallResponse.multiCallReturn.item;
+        if ($scope.category) {
+            client.login(config.storeUsername, config.storePassword).done(function () {
+                client.categoryAssignedProducts($scope.category.category_id).done(function (json) {
+                    jsonPart = json.callResponse.callReturn.item;
                     jsonPart = (_.isObject(jsonPart) && !_.isArray(jsonPart)) ? [{item: jsonPart.item}] : jsonPart;
 
-                    _.each(jsonPart, function (item) {
-                        var product = mapper.build(item);
-                        $rootScope.products.push(product);
+                    productIds = _.map(jsonPart, function (item) {
+                        return item.item[0].value.text;
                     });
-                    client.productMediaList(productIds).done(function (json) {
+
+                    client.productInfo(productIds).done(function (json) {
                         jsonPart = json.multiCallResponse.multiCallReturn.item;
                         jsonPart = (_.isObject(jsonPart) && !_.isArray(jsonPart)) ? [{item: jsonPart.item}] : jsonPart;
 
-                        _.each(jsonPart, function (item, index) {
-                            if (item.item) {
-                                var mediaInfo = mapper.build(item.item);
-                                $rootScope.products[index]["image"] = mediaInfo;
-                            }
+                        _.each(jsonPart, function (item) {
+                            var product = mapper.build(item);
+                            $rootScope.products.push(product);
                         });
-
-                        client.productStockList(productIds).done(function (json) {
-                            jsonPart = json.callResponse.callReturn.item;
+                        client.productMediaList(productIds).done(function (json) {
+                            jsonPart = json.multiCallResponse.multiCallReturn.item;
                             jsonPart = (_.isObject(jsonPart) && !_.isArray(jsonPart)) ? [{item: jsonPart.item}] : jsonPart;
 
                             _.each(jsonPart, function (item, index) {
-                                var stockInfo = mapper.build(item);
-                                $rootScope.products[index]["inventory"] = stockInfo;
+                                if (item.item) {
+                                    var mediaInfo = mapper.build(item.item);
+                                    $rootScope.products[index]["image"] = mediaInfo;
+                                }
                             });
+
+                            client.productStockList(productIds).done(function (json) {
+                                jsonPart = json.callResponse.callReturn.item;
+                                jsonPart = (_.isObject(jsonPart) && !_.isArray(jsonPart)) ? [{item: jsonPart.item}] : jsonPart;
+
+                                _.each(jsonPart, function (item, index) {
+                                    var stockInfo = mapper.build(item);
+                                    $rootScope.products[index]["inventory"] = stockInfo;
+                                });
+                            });
+                            $rootScope.$apply();
                         });
-                        $rootScope.$apply();
                     });
                 });
             });
-        });
+        } else {
+            $location.path("/");
+        }
     });
