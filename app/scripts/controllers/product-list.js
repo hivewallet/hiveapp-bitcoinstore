33bb3b1186638c64408c68c2a072f4113dbc5d36
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-08-13 16:42:15 +0200

    fixed parsing collection when only 1 element is present

--- app/scripts/controllers/product-list.js
+++ app/scripts/controllers/product-list.js
@@ -2,7 +2,7 @@
 
 angular.module("hiveBitcoinstoreApp")
     .controller("ProductListCtrl", function ($scope, $rootScope, $routeParams, config, mapper) {
-        var productIds, item;
+        var productIds, item, jsonPart;
         $scope.categoryId = $routeParams.categoryId;
         $scope.currentPage = parseInt($routeParams.page || 1);
         $rootScope.products = [];
@@ -10,18 +10,26 @@ angular.module("hiveBitcoinstoreApp")
         var client = new MagentoSoapClient(config.storeUrl);
         client.login(config.storeUsername, config.storePassword).done(function () {
             client.categoryAssignedProducts($scope.categoryId).done(function (json) {
-                productIds = _.map(json.callResponse.callReturn.item, function (item) {
+                jsonPart = json.callResponse.callReturn.item;
+                jsonPart = _.isObject(jsonPart) ? [{item: jsonPart.item}] : jsonPart;
+
+                productIds = _.map(jsonPart, function (item) {
                     return item.item[0].value.text;
                 });
 
                 client.productInfo(productIds).done(function (json) {
-                    _.each(json.multiCallResponse.multiCallReturn.item, function (item) {
+                    jsonPart = json.multiCallResponse.multiCallReturn.item;
+                    jsonPart = _.isObject(jsonPart) ? [{item: jsonPart.item}] : jsonPart;
+
+                    _.each(jsonPart, function (item) {
                         item = mapper.build(item);
                         $rootScope.products.push(item)
                     });
-
                     client.productMediaList(productIds).done(function (json) {
-                        _.each(json.multiCallResponse.multiCallReturn.item, function (item, index) {
+                        jsonPart = json.multiCallResponse.multiCallReturn.item;
+                        jsonPart = _.isObject(jsonPart) ? [{item: jsonPart.item}] : jsonPart;
+
+                        _.each(jsonPart, function (item, index) {
                             if (item.item) {
                                 item = mapper.build(item.item);
                                 $rootScope.products[index]["image"] = item;
@@ -29,7 +37,10 @@ angular.module("hiveBitcoinstoreApp")
                         });
 
                         client.productStockList(productIds).done(function (json) {
-                            _.each(json.callResponse.callReturn.item, function (item, index) {
+                            jsonPart = json.callResponse.callReturn.item;
+                            jsonPart = _.isObject(jsonPart) ? [{item: jsonPart.item}] : jsonPart;
+
+                            _.each(jsonPart, function (item, index) {
                                 item = mapper.build(item);
                                 $rootScope.products[index]["inventory"] = item;
                             });
