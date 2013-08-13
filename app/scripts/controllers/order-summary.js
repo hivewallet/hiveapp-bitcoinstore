37139ff6821b3029b01ade366f18ff7664ec109e
Szymon Nowak <szimek@gmail.com>
2013-08-13 18:22:26 +0200

    Payment sort of works.

--- app/scripts/controllers/order-summary.js
+++ app/scripts/controllers/order-summary.js
@@ -1,12 +1,6 @@
 "use strict";
 
 angular.module("hiveBitcoinstoreApp")
-    .controller("OrderSummaryCtrl", function ($scope, $rootScope, $routeParams, $location) {
-        $scope.categoryId = $routeParams.categoryId;
-        $scope.productId = $routeParams.productId;
-
-        $scope.product = _.findWhere($rootScope.products, {product_id: $scope.productId});
-        if (!$scope.product) {
-            $location.path("/categories/" + $scope.categoryId + "/products");
-        }
+    .controller("OrderSummaryCtrl", function ($scope, $routeParams) {
+        $scope.orderId = $routeParams.orderId;
     });
