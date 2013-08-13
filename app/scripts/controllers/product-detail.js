d55c698cdfc1e122b7120fa0b8545ba60fd24258
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-08-13 15:03:00 +0200

    if product is not in the cache, redirect to list

--- app/scripts/controllers/product-detail.js
+++ app/scripts/controllers/product-detail.js
@@ -1,12 +1,15 @@
 "use strict";
 
 angular.module("hiveBitcoinstoreApp")
-    .controller("ProductDetailCtrl", function ($scope, $rootScope, $routeParams, config, mapper) {
+    .controller("ProductDetailCtrl", function ($scope, $rootScope, $routeParams, $location, config, mapper) {
         $scope.categoryId = $routeParams.categoryId;
         $scope.productId = $routeParams.productId;
 
-        // TODO: if not found fetch from API
         $scope.product = _.findWhere($rootScope.products, {product_id: $scope.productId});
+        if (!$scope.product) {
+            // TODO: fetch from API
+            $location.path("/categories/" + $scope.categoryId + "/products");
+        }
 
         $scope.buy = function (productId) {
             alert("You bought " + productId);
