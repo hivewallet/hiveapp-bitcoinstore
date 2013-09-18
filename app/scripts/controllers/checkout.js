eb030858d1cc5230c8000275d944d30a6b20f1a6
Szymon Nowak <szimek@gmail.com>
2013-09-18 09:52:55 +0200

    Update checkout page and disable calling 'cart.order'.

--- app/scripts/controllers/checkout.js
+++ app/scripts/controllers/checkout.js
@@ -20,27 +20,35 @@ angular.module('hiveBitcoinstoreApp')
 
             bitcoin.sendCoins(payment.address, payment.amount, function (success, hash) {
                 if (success) {
-                    async.waterfall([
-                        function (callback) {
-                            client.login(config.storeUsername, config.storePassword)
-                                .done(function () { callback(null); })
-                                .fail(function () { callback(arguments); });
-                        },
-                        // TODO call untill it stops returning an error
-                        function (callback) {
-                            client.cartOrder(cart.id)
-                                .done(function (orderId) { callback(null, orderId); })
-                                .fail(function () { callback(arguments); });
-                        }
-                    ], function (err, orderId) {
-                        if (err) {
-                            $rootScope.errorHandler.apply($rootScope, err);
-                        } else {
-                            // Redirect to checkout page
-                            $location.path('/orders/' + orderId).replace();
-                            $scope.$apply(); // Force path change
-                        }
-                    });
+                    // Redirect to the order summary page
+                    $location.path('/orders/' + cart.id).replace();
+                    $scope.$apply(); // Force path change
+
+                    // TODO:
+                    // Calling "cart.order" should be retried until payment is processed.
+                    // However, it's disabled for the presentation.
+
+                    // async.waterfall([
+                    //     function (callback) {
+                    //         client.login(config.storeUsername, config.storePassword)
+                    //             .done(function () { callback(null); })
+                    //             .fail(function () { callback(arguments); });
+                    //     },
+                    //     // TODO call untill it stops returning an error
+                    //     function (callback) {
+                    //         client.cartOrder(cart.id)
+                    //             .done(function (orderId) { callback(null, orderId); })
+                    //             .fail(function () { callback(arguments); });
+                    //     }
+                    // ], function (err, orderId) {
+                    //     if (err) {
+                    //         $rootScope.errorHandler.apply($rootScope, err);
+                    //     } else {
+                    //         // Redirect to the order summary page
+                    //         $location.path('/orders/' + orderId).replace();
+                    //         $scope.$apply(); // Force path change
+                    //     }
+                    // });
                 } else {
                     $rootScope.errorHandler.apply($rootScope, [{}, 0, ""]);
                 }
