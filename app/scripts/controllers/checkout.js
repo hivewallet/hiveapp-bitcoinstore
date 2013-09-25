aa9572eaa5a67425819e9698703bb62a31695d31
Szymon Nowak <szimek@gmail.com>
2013-09-25 09:31:33 +0200

    Add untested retry (10 times with 5s delay) for 'cart.order' call.

--- app/scripts/controllers/checkout.js
+++ app/scripts/controllers/checkout.js
@@ -8,10 +8,12 @@ angular.module('btcstore.controllers')
     '$routeParams',
     '$location',
     '$filter',
+    '$q',
+    '$timeout',
     'API_SERVER',
     'API_USERNAME',
     'API_PASSWORD',
-    function ($scope, $rootScope, $routeParams, $location, $filter, API_SERVER, API_USERNAME, API_PASSWORD) {
+    function ($scope, $rootScope, $routeParams, $location, $filter, $q, $timeout, API_SERVER, API_USERNAME, API_PASSWORD) {
         if (!$rootScope.cart) {
             $location.path('/').replace();
             $scope.$apply(); // Force path change
@@ -28,37 +30,53 @@ angular.module('btcstore.controllers')
             // Disable form button and display info about payment being processed
             $scope.loading = true;
 
+            // Remove error message if there already is any
+            $rootScope.errorHandler.apply($rootScope);
+
             bitcoin.sendCoins(payment.address, payment.amount, function (success, hash) {
                 if (success) {
-                    // Redirect to the order summary page
-                    $location.path('/orders/' + cart.id).replace();
-                    $scope.$apply(); // Force path change
+                    async.waterfall([
+                        function (callback) {
+                            client.login(API_USERNAME, API_PASSWORD)
+                                .done(function () { callback(null); })
+                                .fail(function () { callback(arguments); });
+                        },
+                        function (callback) {
+                            function order(options) {
+                                // Uses jQuery deferred API
+                                client.cartOrder(cart.id)
+                                    .done(function (orderId) {
+                                        callback(null, orderId);
+                                    })
+                                    .fail(function () {
+                                        if (options.retries > 0) {
+                                            $timeout(function () {
+                                                order({
+                                                    retries: options.retries - 1,
+                                                    delay: 3000
+                                                });
+                                            }, options.delay);
+                                        } else {
+                                            callback(arguments);
+                                        }
+                                    });
+                            }
 
-                    // TODO:
-                    // Calling "cart.order" should be retried until payment is processed.
-                    // However, it's disabled for the presentation.
-
-                    // async.waterfall([
-                    //     function (callback) {
-                    //         client.login(API_USERNAME, API_PASSWORD)
-                    //             .done(function () { callback(null); })
-                    //             .fail(function () { callback(arguments); });
-                    //     },
-                    //     // TODO call untill it stops returning an error
-                    //     function (callback) {
-                    //         client.cartOrder(cart.id)
-                    //             .done(function (orderId) { callback(null, orderId); })
-                    //             .fail(function () { callback(arguments); });
-                    //     }
-                    // ], function (err, orderId) {
-                    //     if (err) {
-                    //         $rootScope.errorHandler.apply($rootScope, err);
-                    //     } else {
-                    //         // Redirect to the order summary page
-                    //         $location.path('/orders/' + orderId).replace();
-                    //         $scope.$apply(); // Force path change
-                    //     }
-                    // });
+                            order({
+                                retries: 10,
+                                delay: 5000
+                            });
+                        }
+                    ], function (err, orderId) {
+                        if (err) {
+                            $scope.loading = false;
+                            $rootScope.errorHandler.apply($rootScope, err);
+                        } else {
+                            // Redirect to the order summary page
+                            $location.path('/orders/' + orderId).replace();
+                            $scope.$apply(); // Force path change
+                        }
+                    });
                 } else {
                     $scope.loading = false;
                     $rootScope.errorHandler.apply($rootScope, [{}, 0, ""]);
